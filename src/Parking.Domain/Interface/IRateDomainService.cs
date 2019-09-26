c10801aff88c301bc3445855fa1193e7eb21894b
Nathan Soares Araujo <nathan.araujo@sp01.local>
2019-09-26 18:52:38 -0300

    versionamento inicial da aplicacao

---
+++ src/Parking.Domain/Interface/IRateDomainService.cs
@@ -0,0 +1,9 @@
+﻿using Parking.Dto;
+
+namespace Parking.Domain.Interface
+{
+    public interface IRateDomainService
+    {
+        bool Create(RateDto rateDto);
+    }
+}
---
+++ src/Parking.Domain/Services/RateDomainService.cs
@@ -0,0 +1,37 @@
+﻿using Parking.Domain.Interface;
+using Parking.Dto;
+
+namespace Parking.Domain.Services
+{
+    public class RateDomainService : IRateDomainService
+    {
+        private ParkingDataContext _context { get; set; }
+
+        public RateDomainService(ParkingDataContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(RateDto rateDto)
+        {
+            try
+            {
+                _context.Rates.Add(new Rate()
+                {
+                    Description = rateDto.Description,
+                    DailyAmount = rateDto.DailyAmount,
+                    HourAmount = rateDto.HourAmount,
+                    OvernightAmount = rateDto.OvernightAmount
+                });
+
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
